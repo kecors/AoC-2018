bad6269076d31c07ebbd54e4c150f6b7cdb7fdf8
Kenneth Collins <dev@kecors.net>
2018-12-05 13:53:19 -0500

    Solve day 5 part 2

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -6,118 +6,121 @@ struct Link {
     right: Option<usize>,
 }
 
-#[derive(Debug)]
-struct Engine {
-    units: Vec<char>,
-    links: Vec<Link>,
-}
-
-impl Engine {
-    fn new(polymer: &str) -> Engine {
-        let units: Vec<char> = polymer.chars().collect();
-        let mut links = Vec::with_capacity(units.len());
-
-        links.push(Link { left: None, right: Some(1) });
-        for j in 1..units.len()-1 {
-            links.push(Link { left: Some(j-1), right: Some(j+1) });
-        }
-        links.push(Link { left: Some(units.len()-2), right: None });
-
-        Engine { units, links }
-    }
-
-    fn solve(&mut self) -> u32 {
-        let mut head = 0;
-        let mut cursor = head;
-
-        while let Some(next) = self.links[cursor].right {
-            if do_react(self.units[cursor], self.units[next]) {
-                match (self.links[cursor].left, self.links[next].right) {
-                    (Some(left), Some(right)) => {
-                        self.links[left].right = Some(right);
-                        self.links[right].left = Some(left);
-
-                        self.links[cursor].left = None;
-                        self.links[cursor].right = None;
-                        self.links[next].left = None;
-                        self.links[next].right = None;
-
-                        cursor = left;
-                    },
-                    (None, Some(right)) => {
-                        self.links[right].left = None;
-
-                        self.links[cursor].right = None;
-                        self.links[next].left = None;
-                        self.links[next].right = None;
-
-                        head = right;
-                        cursor = head;
-                    },
-                    (Some(left), None) => {
-                        self.links[left].right = None;
-
-                        self.links[cursor].left = None;
-                        self.links[cursor].right = None;
-                        self.links[next].left = None;
-
-                        break;
-                    },
-                    (None, None) => {
-                        return 0;
-                    }
-                }
-            } else {
-                cursor = next;
-            }
-        }
-
-        let mut remaining_unit_count = 1;
-
-        cursor = head;
-        while let Some(next) = self.links[cursor].right {
-            remaining_unit_count += 1;
-            cursor = next;
-        }
-
-        remaining_unit_count
-    }
-
-    #[allow(dead_code)]
-    fn display(&self) {
-        let mut cursor = 0;
-
-        while let None = self.links[cursor].right {
-            cursor += 1;
-        }
-
-        println!("[{}] {:?} {}", cursor, self.links[cursor], self.units[cursor]);
-
-        while let Some(next) = self.links[cursor].right {
-            println!("[{}] {:?} {}", next, self.links[next], self.units[next]);
-            cursor = next;
-        }
-    }
-}
-
 fn do_react(left: char, right: char) -> bool {
     if left == right {
         return false;
     }
-    if left.to_uppercase().to_string() == right.to_uppercase().to_string() {
+    if left.to_ascii_uppercase() == right.to_ascii_uppercase() {
         return true;
     }
 
     false
 }
 
+fn react_polymer(units: &[char]) -> u32 {
+    let mut links = Vec::with_capacity(units.len());
+
+    links.push(Link {
+        left: None,
+        right: Some(1),
+    });
+    for j in 1..units.len() - 1 {
+        links.push(Link {
+            left: Some(j - 1),
+            right: Some(j + 1),
+        });
+    }
+    links.push(Link {
+        left: Some(units.len() - 2),
+        right: None,
+    });
+
+    let mut head = 0;
+    let mut cursor = head;
+
+    while let Some(next) = links[cursor].right {
+        if do_react(units[cursor], units[next]) {
+            match (links[cursor].left, links[next].right) {
+                (Some(left), Some(right)) => {
+                    links[left].right = Some(right);
+                    links[right].left = Some(left);
+
+                    links[cursor].left = None;
+                    links[cursor].right = None;
+                    links[next].left = None;
+                    links[next].right = None;
+
+                    cursor = left;
+                }
+                (None, Some(right)) => {
+                    links[right].left = None;
+
+                    links[cursor].right = None;
+                    links[next].left = None;
+                    links[next].right = None;
+
+                    head = right;
+                    cursor = head;
+                }
+                (Some(left), None) => {
+                    links[left].right = None;
+
+                    links[cursor].left = None;
+                    links[cursor].right = None;
+                    links[next].left = None;
+
+                    break;
+                }
+                (None, None) => {
+                    return 0;
+                }
+            }
+        } else {
+            cursor = next;
+        }
+    }
+
+    let mut remaining_unit_count = 1;
+
+    cursor = head;
+    while let Some(next) = links[cursor].right {
+        remaining_unit_count += 1;
+        cursor = next;
+    }
+
+    remaining_unit_count
+}
+
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
 
-    let mut engine = Engine::new(&input.trim());
+    let units: Vec<char> = input.trim().chars().collect();
 
-    let part1 = engine.solve();
+    let remaining_unit_count = react_polymer(&units);
+    println!(
+        "Part 1: {} units remain after fully reacting the polymer",
+        remaining_unit_count
+    );
 
-    println!("Part 1: {} units remain after fully reacting the polymer", part1);
+    let mut unit_types: Vec<char> = units.iter().map(|x| x.to_ascii_lowercase()).collect();
+    unit_types.sort();
+    unit_types.dedup();
+
+    let mut min_remaining_unit_count = u32::max_value();
+    for unit_type in unit_types {
+        let filtered_units: Vec<char> = units
+            .clone()
+            .into_iter()
+            .filter(|x| x.to_ascii_lowercase() != unit_type)
+            .collect();
+        let remaining_unit_count = react_polymer(&filtered_units);
+        if remaining_unit_count < min_remaining_unit_count {
+            min_remaining_unit_count = remaining_unit_count;
+        }
+    }
+    println!(
+        "Part 2: the length of the shortest polymer is {}",
+        min_remaining_unit_count
+    );
 }
