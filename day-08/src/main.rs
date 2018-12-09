9e131940fa4005c8b3be30bc30b8cf7ddc15e49e
Kenneth Collins <dev@kecors.net>
2018-12-09 15:03:37 -0500

    Solve day 8 part 2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -2,6 +2,7 @@ use std::io::{stdin, Read};
 
 #[derive(Debug)]
 struct Node {
+    index: usize,
     child_quantity: u32,
     metadata_quantity: u32,
     children: Vec<usize>,
@@ -9,10 +10,11 @@ struct Node {
 }
 
 impl Node {
-    fn new(child_quantity: u32, metadata_quantity: u32) -> Node {
+    fn new(index: usize, child_quantity: u32, metadata_quantity: u32) -> Node {
         let children = Vec::new();
         let metadata = Vec::new();
         Node {
+            index,
             child_quantity,
             metadata_quantity,
             children,
@@ -25,16 +27,19 @@ struct Engine {
     numbers: Vec<u32>,
     numbers_index: usize,
     nodes: Vec<Node>,
+    next_node_index: usize,
 }
 
 impl Engine {
     fn new(numbers: Vec<u32>) -> Engine {
         let numbers_index = 0;
         let nodes = Vec::new();
+        let next_node_index = 0;
         Engine {
             numbers,
             numbers_index,
             nodes,
+            next_node_index,
         }
     }
 
@@ -44,19 +49,20 @@ impl Engine {
         let metadata_quantity = self.numbers[self.numbers_index];
         self.numbers_index += 1;
 
-        let node = Node::new(child_quantity, metadata_quantity);
+        let own_node_index = self.next_node_index;
+        self.next_node_index += 1;
+        let node = Node::new(own_node_index, child_quantity, metadata_quantity);
         self.nodes.push(node);
-        let own_nodes_index = self.nodes.len() - 1;
 
         for _child in 0..child_quantity {
-            let child_nodes_index = self.nodes.len();
-            self.nodes[own_nodes_index].children.push(child_nodes_index);
+            let child_node_index = self.next_node_index;
+            self.nodes[own_node_index].children.push(child_node_index);
             self.read_node();
         }
         for _metadata in 0..metadata_quantity {
             let metadata = self.numbers[self.numbers_index];
             self.numbers_index += 1;
-            self.nodes[own_nodes_index].metadata.push(metadata);
+            self.nodes[own_node_index].metadata.push(metadata);
         }
     }
 
@@ -70,6 +76,29 @@ impl Engine {
         }
 
         sum
+    }
+
+    fn root_node_value(&mut self) -> u32 {
+        let mut values = vec![0; self.nodes.len()];
+
+        for node in self.nodes.iter().rev() {
+            if node.child_quantity == 0 {
+                values[node.index] = node.metadata.iter().sum();
+            } else {
+                let metadata: Vec<usize> = node
+                    .metadata
+                    .iter()
+                    .filter(|&&x| x > 0 && x as usize <= node.children.len())
+                    .map(|&x| x as usize - 1)
+                    .collect();
+                for metadatum in metadata {
+                    let child_index: usize = node.children[metadatum];
+                    values[node.index] += values[child_index];
+                }
+            }
+        }
+
+        values[0]
     }
 }
 
@@ -88,4 +117,7 @@ fn main() {
 
     let part1 = engine.metadata_sum();
     println!("Part 1: the sum of all metadata entries is {}", part1);
+
+    let part2 = engine.root_node_value();
+    println!("Part 2: the value of the root node is {}", part2);
 }
