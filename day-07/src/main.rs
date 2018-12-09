83f906747b02450f4193dcd443a0e6aa3061aa25
Kenneth Collins <dev@kecors.net>
2018-12-09 11:15:01 -0500

    Solve day 7 part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -3,12 +3,52 @@ use std::collections::{HashMap, HashSet};
 use std::io::{stdin, Read};
 
 #[derive(Debug)]
+struct Delay {
+    seconds: u32,
+}
+
+impl Into<Delay> for char {
+    fn into(self) -> Delay {
+        let seconds = match self {
+            'A' => 61,
+            'B' => 62,
+            'C' => 63,
+            'D' => 64,
+            'E' => 65,
+            'F' => 66,
+            'G' => 67,
+            'H' => 68,
+            'I' => 69,
+            'J' => 70,
+            'K' => 71,
+            'L' => 72,
+            'M' => 73,
+            'N' => 74,
+            'O' => 75,
+            'P' => 76,
+            'Q' => 77,
+            'R' => 78,
+            'S' => 79,
+            'T' => 80,
+            'U' => 81,
+            'V' => 82,
+            'W' => 83,
+            'X' => 84,
+            'Y' => 85,
+            'Z' => 86,
+            _ => 0,
+        };
+        Delay { seconds }
+    }
+}
+
+#[derive(Debug)]
 struct Requirement {
     before: char,
     after: char,
 }
 
-fn solve(requirements: &[Requirement]) -> String {
+fn solve(workers: usize, requirements: &[Requirement]) -> (String, u32) {
     let mut letter_hs = HashSet::new();
     for requirement in requirements.iter() {
         letter_hs.insert(requirement.before);
@@ -17,39 +57,78 @@ fn solve(requirements: &[Requirement]) -> String {
     let mut letters: Vec<char> = letter_hs.into_iter().collect();
     letters.sort();
 
-    let mut obstructions: HashMap<char, HashSet<char>> = HashMap::new();
+    let mut obstructions: HashMap<char, Option<HashSet<char>>> = HashMap::new();
     for &letter in letters.iter() {
-        obstructions.insert(letter, HashSet::new());
+        obstructions.insert(letter, Some(HashSet::new()));
     }
     for requirement in requirements.iter() {
-        if let Some(hs) = obstructions.get_mut(&requirement.after) {
-            hs.insert(requirement.before);
+        if let Some(hs_opt) = obstructions.get_mut(&requirement.after) {
+            if let Some(hs) = hs_opt {
+                hs.insert(requirement.before);
+            }
         }
     }
 
-    let mut result: Vec<char> = Vec::new();
+    let mut seconds = 0;
+    let mut delays: Vec<Option<(u32, char)>> = vec![None; 5];
+    let mut order: Vec<char> = Vec::new();
 
-    while result.len() < letters.len() {
-        let mut letters_iter = letters.iter();
-        let next_letter = loop {
-            if let Some(letter) = letters_iter.next() {
-                if result.contains(&letter) {
+    loop {
+        for worker in 0..workers {
+            if None != delays[worker] {
+                continue;
+            }
+            let mut next_letter_opt = None;
+            for letter in letters.iter() {
+                if order.contains(&letter) {
                     continue;
                 }
-                if obstructions[letter].is_empty() {
-                    break *letter;
+                if let Some(obstruction_opt) = obstructions.get_mut(&letter) {
+                    if let Some(obstruction) = obstruction_opt {
+                        if obstruction.is_empty() {
+                            next_letter_opt = Some(*letter);
+                            *obstruction_opt = None;
+                            break;
+                        }
+                    }
                 }
             }
-        };
-        result.push(next_letter);
-        for &letter in letters.iter() {
-            if let Some(hs) = obstructions.get_mut(&letter) {
-                hs.remove(&next_letter);
+            if let Some(next_letter) = next_letter_opt {
+                let delay: Delay = next_letter.into();
+                delays[worker] = Some((delay.seconds, next_letter));
+            }
+        }
+
+        if order.len() == letters.len() {
+            break;
+        }
+
+        seconds += 1;
+
+        for worker in 0..workers {
+            if let Some((mut delay, next_letter)) = delays[worker] {
+                delay -= 1;
+
+                if delay == 0 {
+                    order.push(next_letter);
+
+                    for &letter in letters.iter() {
+                        if let Some(hs_opt) = obstructions.get_mut(&letter) {
+                            if let Some(hs) = hs_opt {
+                                hs.remove(&next_letter);
+                            }
+                        }
+                    }
+
+                    delays[worker] = None;
+                } else {
+                    delays[worker] = Some((delay, next_letter));
+                }
             }
         }
     }
 
-    result.iter().collect()
+    (order.iter().collect(), seconds)
 }
 
 fn main() {
@@ -67,9 +146,12 @@ fn main() {
         requirements.push(Requirement { before, after });
     }
 
-    let part1 = solve(&requirements);
+    let (order, _seconds) = solve(1, &requirements);
     println!(
         "Part 1: the steps should be completed in this order: {}",
-        part1
+        order
     );
+
+    let (_order, seconds) = solve(5, &requirements);
+    println!("Part 2: It will take {} seconds to complete", seconds);
 }
