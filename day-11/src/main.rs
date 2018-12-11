87946aee610fa2cbbd0702d6fabdbbf3b2dc5e5a
Kenneth Collins <dev@kecors.net>
2018-12-11 10:21:01 -0500

    Solve day 11 part 2

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,6 +1,6 @@
 use std::io::{stdin, Read};
 
-fn solve(serial_number: i32) -> (usize, usize) {
+fn solve(serial_number: i32, min_dial: usize, max_dial: usize) -> ((usize, usize, usize)) {
     let mut grid = Vec::new();
 
     for y0 in 0..300 {
@@ -17,25 +17,40 @@ fn solve(serial_number: i32) -> (usize, usize) {
         grid.push(row);
     }
 
+    let mut memoized_total_power = vec![vec![None; 300]; 300];
     let mut largest_total_power = i32::min_value();
-    let mut largest_coordinate = None;
+    let mut solution = None;
 
-    for y0 in 0..298 {
-        for x0 in 0..298 {
-            let mut total_power = 0;
-            for yn in 0..3 {
-                for xn in 0..3 {
-                    total_power += grid[y0 + yn][x0 + xn];
+    for dial in min_dial..=max_dial {
+        for y0 in 0..=(300 - dial) {
+            for x0 in 0..=(300 - dial) {
+                let mut total_power = 0;
+                if let Some(memo) = memoized_total_power[y0][x0] {
+                    total_power += memo;
+                    for yn in 0..(dial - 1) {
+                        total_power += grid[y0 + yn][x0 + dial - 1];
+                    }
+                    for xn in 0..dial {
+                        total_power += grid[y0 + dial - 1][x0 + xn];
+                    }
+                } else {
+                    for yn in 0..dial {
+                        for xn in 0..dial {
+                            total_power += grid[y0 + yn][x0 + xn];
+                        }
+                    }
                 }
-            }
-            if total_power > largest_total_power {
-                largest_total_power = total_power;
-                largest_coordinate = Some((x0 + 1, y0 + 1));
+                memoized_total_power[y0][x0] = Some(total_power);
+
+                if total_power > largest_total_power {
+                    largest_total_power = total_power;
+                    solution = Some((x0 + 1, y0 + 1, dial));
+                }
             }
         }
     }
 
-    largest_coordinate.unwrap()
+    solution.unwrap()
 }
 
 fn main() {
@@ -43,8 +58,13 @@ fn main() {
     stdin().read_to_string(&mut input).unwrap();
 
     let serial_number: i32 = input.trim().parse().unwrap();
-    println!("serial_number = {}", serial_number);
 
-    let part1 = solve(serial_number);
-    println!("Part 1: the X,Y coordinate of the top-left fuel cell of the 3x3 square with the largest total power is {:?}", part1);
+    let (x, y, _dial) = solve(serial_number, 3, 3);
+    println!("Part 1: the X,Y coordinate of the top-left fuel cell of the 3x3 square with the largest total power is {},{}", x, y);
+
+    let (x, y, dial) = solve(serial_number, 1, 300);
+    println!(
+        "Part 2: the X,Y,size identifier of the square with the largest total power is {},{},{}",
+        x, y, dial
+    );
 }
