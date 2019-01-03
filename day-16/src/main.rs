af1b99585eb7ec26c3198f8f382768fd8688f24d
Kenneth Collins <dev@kecors.net>
2019-01-03 12:19:30 -0500

    Solve day 16 part 2

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -1,24 +1,20 @@
 use pom::parser::*;
 use pom::Error;
+use std::collections::{HashMap, HashSet};
 use std::io::{stdin, Read};
 use std::str;
 
-type Value = u8;
-type Opcode = u8;
-type Register = usize;
-type RegisterOrValue = usize;
-
 #[derive(Debug, Default, Clone, Eq, PartialEq)]
 struct Contents {
-    registers: Vec<Value>,
+    registers: Vec<u32>,
 }
 
 #[derive(Debug)]
 struct Instruction {
-    opcode: Opcode,
-    a: RegisterOrValue,
-    b: RegisterOrValue,
-    c: Register,
+    opcode: usize,
+    a: usize,
+    b: usize,
+    c: usize,
 }
 
 #[derive(Debug)]
@@ -33,35 +29,107 @@ struct Program {
     instructions: Vec<Instruction>,
 }
 
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
+enum Operator {
+    Addr,
+    Addi,
+    Mulr,
+    Muli,
+    Banr,
+    Bani,
+    Borr,
+    Bori,
+    Setr,
+    Seti,
+    Gtir,
+    Gtri,
+    Gtrr,
+    Eqir,
+    Eqri,
+    Eqrr,
+}
+
 #[derive(Debug)]
 struct Engine {
     samples: Vec<Sample>,
+    sample_valid_operators: HashMap<usize, HashSet<Operator>>,
+    opcodes: Vec<Option<Operator>>,
     program: Program,
 }
 
 impl Engine {
-    fn sample_behaviour_count(&self) -> u32 {
-        let mut sample_count = 0;
+    fn generate_sample_valid_operators(&mut self) {
+        use self::Operator::*;
 
-        for sample in self.samples.iter() {
-            let ops = [
-                addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir,
-                eqri, eqrr,
-            ];
-            let mut op_count = 0;
+        let operators = [
+            Addr, Addi, Mulr, Muli, Banr, Bani, Borr, Bori, Setr, Seti, Gtir, Gtri, Gtrr, Eqir,
+            Eqri, Eqrr,
+        ];
 
-            for op in ops.iter() {
-                if op(&sample.before, &sample.instruction) == sample.after {
-                    op_count += 1;
+        for (index, sample) in self.samples.iter().enumerate() {
+            for operator in operators.iter() {
+                if operate(*operator, &sample.before, &sample.instruction) == sample.after {
+                    let svo = self
+                        .sample_valid_operators
+                        .entry(index)
+                        .or_insert_with(HashSet::new);
+                    svo.insert(*operator);
+                }
+            }
+        }
+    }
+
+    fn generate_opcodes(&mut self) {
+        let mut work_remains_flag = true;
+
+        while work_remains_flag {
+            work_remains_flag = false;
+
+            for (sample_index, operators) in self.sample_valid_operators.iter() {
+                match operators.len() {
+                    0 => {}
+                    1 => {
+                        let opcode = self.samples[*sample_index].instruction.opcode;
+                        let operator = *operators.iter().next().unwrap();
+                        match self.opcodes[opcode] {
+                            None => {
+                                self.opcodes[opcode] = Some(operator);
+                            }
+                            Some(operator_) => {
+                                if operator_ != operator {
+                                    panic!("One opcode associated with multiple operators");
+                                }
+                            }
+                        }
+                    }
+                    _ => {
+                        work_remains_flag = true;
+                    }
                 }
             }
 
-            if op_count >= 3 {
-                sample_count += 1;
+            for (_, operators) in self.sample_valid_operators.iter_mut() {
+                self.opcodes.iter().for_each(|x| {
+                    if let Some(opcode) = x {
+                        operators.remove(opcode);
+                    }
+                });
+            }
+        }
+    }
+
+    fn run_program(&self) -> Contents {
+        let mut contents = Contents {
+            registers: vec![0; 4],
+        };
+
+        for instruction in self.program.instructions.iter() {
+            if let Some(opcode) = self.opcodes[instruction.opcode] {
+                contents = operate(opcode, &contents, &instruction);
             }
         }
 
-        sample_count
+        contents
     }
 }
 
@@ -69,12 +137,12 @@ fn space<'a>() -> Parser<'a, u8, ()> {
     one_of(b" \t\r\n").repeat(0..).discard()
 }
 
-fn number<'a>() -> Parser<'a, u8, u8> {
+fn number<'a>() -> Parser<'a, u8, u32> {
     let number = (one_of(b"123456789") - one_of(b"0123456789").repeat(0..2)) | sym(b'0');
     number
         .collect()
         .convert(str::from_utf8)
-        .convert(|s| u8::from_str_radix(s, 10))
+        .convert(|s| u32::from_str_radix(s, 10))
 }
 
 fn contents<'a>() -> Parser<'a, u8, Contents> {
@@ -89,7 +157,7 @@ fn contents<'a>() -> Parser<'a, u8, Contents> {
 
 fn instruction<'a>() -> Parser<'a, u8, Instruction> {
     ((space() * number()).repeat(4)).map(|numbers| Instruction {
-        opcode: numbers[0],
+        opcode: numbers[0] as usize,
         a: numbers[1] as usize,
         b: numbers[2] as usize,
         c: numbers[3] as usize,
@@ -119,173 +187,109 @@ fn program<'a>() -> Parser<'a, u8, Program> {
 }
 
 fn engine<'a>() -> Parser<'a, u8, Engine> {
-    (samples() + program()).map(|(samples, program)| Engine { samples, program })
+    (samples() + program()).map(|(samples, program)| Engine {
+        samples,
+        sample_valid_operators: HashMap::new(),
+        opcodes: vec![None; 16],
+        program,
+    })
 }
 
-fn addr(contents: &Contents, instruction: &Instruction) -> Contents {
+fn operate(operator: Operator, contents: &Contents, instruction: &Instruction) -> Contents {
+    use self::Operator::*;
+
     let mut result: Contents = (*contents).clone();
 
-    let a = contents.registers[instruction.a];
-    let b = contents.registers[instruction.b];
-    result.registers[instruction.c] = a + b;
-
-    result
-}
-
-fn addi(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    let a = contents.registers[instruction.a];
-    let b = instruction.b as u8;
-    result.registers[instruction.c] = a + b;
-
-    result
-}
-
-fn mulr(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    let a = contents.registers[instruction.a];
-    let b = contents.registers[instruction.b];
-    result.registers[instruction.c] = a * b;
-
-    result
-}
-
-fn muli(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    let a = contents.registers[instruction.a];
-    let b = instruction.b as u8;
-    result.registers[instruction.c] = a * b;
-
-    result
-}
-
-fn banr(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    let a = contents.registers[instruction.a];
-    let b = contents.registers[instruction.b];
-    result.registers[instruction.c] = a & b;
-
-    result
-}
-
-fn bani(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    let a = contents.registers[instruction.a];
-    let b = instruction.b as u8;
-    result.registers[instruction.c] = a & b;
-
-    result
-}
-
-fn borr(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    let a = contents.registers[instruction.a];
-    let b = contents.registers[instruction.b];
-    result.registers[instruction.c] = a | b;
-
-    result
-}
-
-fn bori(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    let a = contents.registers[instruction.a];
-    let b = instruction.b as u8;
-    result.registers[instruction.c] = a | b;
-
-    result
-}
-
-fn setr(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    let a = contents.registers[instruction.a];
-    result.registers[instruction.c] = a;
-
-    result
-}
-
-fn seti(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    result.registers[instruction.c] = instruction.a as u8;
-
-    result
-}
-
-fn gtir(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    if instruction.a as u8 > contents.registers[instruction.b] {
-        result.registers[instruction.c] = 1;
-    } else {
-        result.registers[instruction.c] = 0;
-    }
-
-    result
-}
-
-fn gtri(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    if contents.registers[instruction.a] > instruction.b as u8 {
-        result.registers[instruction.c] = 1;
-    } else {
-        result.registers[instruction.c] = 0;
-    }
-
-    result
-}
-
-fn gtrr(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    if contents.registers[instruction.a] > contents.registers[instruction.b] {
-        result.registers[instruction.c] = 1;
-    } else {
-        result.registers[instruction.c] = 0;
-    }
-
-    result
-}
-
-fn eqir(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    if instruction.a as u8 == contents.registers[instruction.b] {
-        result.registers[instruction.c] = 1;
-    } else {
-        result.registers[instruction.c] = 0;
-    }
-
-    result
-}
-
-fn eqri(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    if contents.registers[instruction.a] == instruction.b as u8 {
-        result.registers[instruction.c] = 1;
-    } else {
-        result.registers[instruction.c] = 0;
-    }
-
-    result
-}
-
-fn eqrr(contents: &Contents, instruction: &Instruction) -> Contents {
-    let mut result: Contents = (*contents).clone();
-
-    if contents.registers[instruction.a] == contents.registers[instruction.b] {
-        result.registers[instruction.c] = 1;
-    } else {
-        result.registers[instruction.c] = 0;
+    match operator {
+        Addr => {
+            let a = contents.registers[instruction.a];
+            let b = contents.registers[instruction.b];
+            result.registers[instruction.c] = a + b;
+        }
+        Addi => {
+            let a = contents.registers[instruction.a];
+            let b = instruction.b as u32;
+            result.registers[instruction.c] = a + b;
+        }
+        Mulr => {
+            let a = contents.registers[instruction.a];
+            let b = contents.registers[instruction.b];
+            result.registers[instruction.c] = a * b;
+        }
+        Muli => {
+            let a = contents.registers[instruction.a];
+            let b = instruction.b as u32;
+            result.registers[instruction.c] = a * b;
+        }
+        Banr => {
+            let a = contents.registers[instruction.a];
+            let b = contents.registers[instruction.b];
+            result.registers[instruction.c] = a & b;
+        }
+        Bani => {
+            let a = contents.registers[instruction.a];
+            let b = instruction.b as u32;
+            result.registers[instruction.c] = a & b;
+        }
+        Borr => {
+            let a = contents.registers[instruction.a];
+            let b = contents.registers[instruction.b];
+            result.registers[instruction.c] = a | b;
+        }
+        Bori => {
+            let a = contents.registers[instruction.a];
+            let b = instruction.b as u32;
+            result.registers[instruction.c] = a | b;
+        }
+        Setr => {
+            let a = contents.registers[instruction.a];
+            result.registers[instruction.c] = a;
+        }
+        Seti => {
+            result.registers[instruction.c] = instruction.a as u32;
+        }
+        Gtir => {
+            if instruction.a as u32 > contents.registers[instruction.b] {
+                result.registers[instruction.c] = 1;
+            } else {
+                result.registers[instruction.c] = 0;
+            }
+        }
+        Gtri => {
+            if contents.registers[instruction.a] > instruction.b as u32 {
+                result.registers[instruction.c] = 1;
+            } else {
+                result.registers[instruction.c] = 0;
+            }
+        }
+        Gtrr => {
+            if contents.registers[instruction.a] > contents.registers[instruction.b] {
+                result.registers[instruction.c] = 1;
+            } else {
+                result.registers[instruction.c] = 0;
+            }
+        }
+        Eqir => {
+            if instruction.a as u32 == contents.registers[instruction.b] {
+                result.registers[instruction.c] = 1;
+            } else {
+                result.registers[instruction.c] = 0;
+            }
+        }
+        Eqri => {
+            if contents.registers[instruction.a] == instruction.b as u32 {
+                result.registers[instruction.c] = 1;
+            } else {
+                result.registers[instruction.c] = 0;
+            }
+        }
+        Eqrr => {
+            if contents.registers[instruction.a] == contents.registers[instruction.b] {
+                result.registers[instruction.c] = 1;
+            } else {
+                result.registers[instruction.c] = 0;
+            }
+        }
     }
 
     result
@@ -295,12 +299,27 @@ fn main() -> Result<(), Error> {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
 
-    let engine = engine().parse(input.as_bytes())?;
+    let mut engine = engine().parse(input.as_bytes())?;
 
-    let count = engine.sample_behaviour_count();
+    engine.generate_sample_valid_operators();
+
+    // For part 1, count samples with three or more valid operators
+    let mut count = 0;
+    for (_, operators) in engine.sample_valid_operators.iter() {
+        if operators.len() >= 3 {
+            count += 1;
+        }
+    }
     println!(
         "Part 1: {} samples behave like three or more opcodes",
         count
+    );
+
+    engine.generate_opcodes();
+    let contents = engine.run_program();
+    println!(
+        "Part 2: the value contained in register 0 after running the test program is {}",
+        contents.registers[0]
     );
 
     Ok(())
