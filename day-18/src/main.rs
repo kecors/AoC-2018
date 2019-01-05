7436b515ff13427c9697728f43ee662e380cab33
Kenneth Collins <dev@kecors.net>
2019-01-05 17:17:16 -0500

    Solve day 18 part 1

---
+++ day-18/src/main.rs
@@ -0,0 +1,154 @@
+use std::io::{stdin, Read};
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+enum Terrain {
+    Open,
+    Tree,
+    Yard,
+}
+
+fn terrain_counts(terrains: &[Terrain]) -> (u32, u32) {
+    let trees = terrains.iter().filter(|&&x| x == Terrain::Tree).count() as u32;
+
+    let yards = terrains.iter().filter(|&&x| x == Terrain::Yard).count() as u32;
+
+    (trees, yards)
+}
+
+struct Engine {
+    area: Vec<Vec<Terrain>>,
+}
+
+impl Engine {
+    fn display(&self) {
+        for y in 0..self.area.len() {
+            for x in 0..self.area[0].len() {
+                print!(
+                    "{}",
+                    match self.area[y][x] {
+                        Terrain::Open => ".",
+                        Terrain::Tree => "|",
+                        Terrain::Yard => "#",
+                    }
+                );
+            }
+            println!();
+        }
+        println!();
+    }
+
+    fn tick(&mut self) {
+        let mut area: Vec<Vec<Terrain>> = Vec::new();
+
+        for y in 0..self.area.len() {
+            let mut row = Vec::new();
+            for x in 0..self.area[0].len() {
+                let adjacent_terrains: Vec<Terrain> = self
+                    .adjacents(x, y)
+                    .iter()
+                    .map(|(x, y)| self.area[*y][*x])
+                    .collect();
+                let (trees, yards) = terrain_counts(&adjacent_terrains);
+                let terrain = match self.area[y][x] {
+                    Terrain::Open => {
+                        if trees >= 3 {
+                            Terrain::Tree
+                        } else {
+                            Terrain::Open
+                        }
+                    }
+                    Terrain::Tree => {
+                        if yards >= 3 {
+                            Terrain::Yard
+                        } else {
+                            Terrain::Tree
+                        }
+                    }
+                    Terrain::Yard => {
+                        if trees >= 1 && yards >= 1 {
+                            Terrain::Yard
+                        } else {
+                            Terrain::Open
+                        }
+                    }
+                };
+                row.push(terrain);
+            }
+            area.push(row);
+        }
+
+        self.area = area;
+    }
+
+    fn adjacents(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
+        let left_edge: bool = x == 0;
+        let right_edge: bool = x == self.area[0].len() - 1;
+        let top_edge: bool = y == 0;
+        let bottom_edge: bool = y == self.area.len() - 1;
+
+        let mut adjacents = Vec::new();
+
+        if !left_edge && !top_edge {
+            adjacents.push((x - 1, y - 1))
+        }
+        if !top_edge {
+            adjacents.push((x, y - 1))
+        }
+        if !right_edge && !top_edge {
+            adjacents.push((x + 1, y - 1))
+        }
+        if !left_edge {
+            adjacents.push((x - 1, y))
+        }
+        if !right_edge {
+            adjacents.push((x + 1, y))
+        }
+        if !left_edge && !bottom_edge {
+            adjacents.push((x - 1, y + 1))
+        }
+        if !bottom_edge {
+            adjacents.push((x, y + 1))
+        }
+        if !bottom_edge && !right_edge {
+            adjacents.push((x + 1, y + 1))
+        }
+
+        adjacents
+    }
+}
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+
+    let mut area: Vec<Vec<Terrain>> = Vec::new();
+    for line in input.lines() {
+        let mut row = Vec::new();
+        for ch in line.chars() {
+            row.push(match ch {
+                '.' => Terrain::Open,
+                '|' => Terrain::Tree,
+                '#' => Terrain::Yard,
+                _ => {
+                    panic!("Unexpected input");
+                }
+            });
+        }
+        area.push(row);
+    }
+
+    let mut engine = Engine { area };
+
+    engine.display();
+    for minute in 1..=10 {
+        engine.tick();
+        println!("minute = {}", minute);
+        engine.display();
+    }
+    let terrains: Vec<Terrain> = engine.area.iter().cloned().flatten().collect();
+    let (trees, yards) = terrain_counts(&terrains);
+    println!(
+        "Part 1: the total resource value of the area after 10 minutes is {}",
+        trees * yards
+    );
+}
